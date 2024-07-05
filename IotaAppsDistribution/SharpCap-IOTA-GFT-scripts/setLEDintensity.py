a7470d0ee760b904e632bd4c5cb9bfdf5e9e9876
boban <bob.anderson.ok@gmail.com>
2024-07-05 13:05:28 -0700

    commit 10

---
+++ IotaAppsDistribution/SharpCap-IOTA-GFT-scripts/setLEDintensity.py
@@ -0,0 +1,19 @@
+import socket, os
+
+def setLEDintensity():
+	HOST = '127.0.0.1'
+	PORT = 33001
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.connect((HOST, PORT))
+		msg = makeMsg('setLEDintensity 650')    # Make the command string MSGLEN in length
+		s.sendall(msg)                          # Send the command
+		_, ans = getResponse(s)                 # Get the response
+		ans = msgTrim(ans.decode("utf-8"))      # Turn bytes into string characters
+		print('Sent:', 'setLEDintensity 650')
+		if ans == "OK":
+			print("Rcvd:", ans)
+		else:
+			print("ERR!:", ans)
+		
+
+setLEDintensity()
