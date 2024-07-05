5d46ea15c770dde9c89987d9fe274f71503c6349
boban <bob.anderson.ok@gmail.com>
2024-07-05 10:02:43 -0700

    commit 7

--- IotaAppsDistribution/SharpCap-IOTA-GFT-scripts/setUTCeventTime.py
+++ IotaAppsDistribution/SharpCap-IOTA-GFT-scripts/setUTCeventTime.py
@@ -5,10 +5,10 @@ def sendUTCeventStartTime():
 	PORT = 33001
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
-		msg = makeMsg('setUTCeventTime 2026-01-25 10:11:12')
-		s.sendall(msg)                      # Send the command
-		_, ans = getResponse(s)             # Get the response
-		ans = msgTrim(ans.decode("utf-8"))  # Turn bytes into string characters
+		msg = makeMsg('setUTCeventTime 2026-01-25 10:11:12') # Make the command string MSGLEN in length
+		s.sendall(msg)                                       # Send the command
+		_, ans = getResponse(s)                              # Get the response
+		ans = msgTrim(ans.decode("utf-8"))                   # Turn bytes into string characters
 		print('Sent:', 'setUTCeventTime 2026-01-25 10:11:12')
 		if ans == "OK":
 			print("Rcvd:", ans)
